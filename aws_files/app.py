7aed1c3bcc553f503336bd6331ac09913328b4eb
Guillermo Herrera Acosta <a01400835@itesm.mx>
2020-04-03 22:31:51 -0600

    aws files updated and pre-formated
    Signed-off-by: Guillermo Herrera Acosta <a01400835@itesm.mx>

---
+++ aws_files/app.py
@@ -0,0 +1,50 @@
+import mysql.connector
+
+import adapter
+import credentials
+
+from mysql.connector import errorcode
+from json import dumps
+
+
+CREDENTIALS = {
+    'user': credentials.USER,
+    'password': credentials.PASSWORD,
+    'host': credentials.HOST,
+    'raise_on_warnings': credentials.RAISE_ON_WARNINGS,
+    'use_pure': credentials.USE_PURE
+}
+
+
+
+def lambda_handler(event, context):
+    try:
+        cnx = mysql.connector.connect(**CREDENTIALS)
+
+        data = event['queryStringParameters']
+        query = adapter.get_query(data)
+
+        cursor = cnx.cursor()
+        cursor.execute(query)
+        output = adapter.get_data(cursor)
+
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Username o password incorrecto")
+            output = "Username o password incorrecto"
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("La base de datos no existe")
+            output = "La base de datos no existe"
+        else:
+            print(err)
+            output = err
+    else:
+        cnx.close()
+        return {
+            "isBase64Encoded": False,
+            "statusCode": 200,
+            "body": dumps(output),
+            "headers": {
+                "Content-Type": "application/json"
+            }
+        }
