cdb97524eeceb606398fbd0af3c8cfdd98105f82
Guillermo Herrera Acosta <a01400835@itesm.mx>
2020-04-12 21:09:07 -0500

    Mysql connection from go instance, package changed to repo's
    Signed-off-by: Guillermo Herrera Acosta <a01400835@itesm.mx>

---
+++ credentials_template.go
@@ -0,0 +1,11 @@
+package collabnotes
+
+const (
+	dummyUSER     = "" //db user
+	dummyPASSWORD = "" //db password
+	dummyHOST     = "" //db endpoint
+	dummyPORT     = "" //db port
+	dummyDBNAME   = "" //db name
+
+	dummyROUTE = USER + ":" + PASSWORD + "@tcp(" + HOST + ":" + PORT + ")/" + DBNAME
+)
